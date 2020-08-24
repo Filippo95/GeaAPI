130fc5ea8859ec5fdaeafa5ebb8aafc6aee73d0f
Filippo Luppi <filippoluppi@MacBook-Pro-di-Filippo.local>
2020-08-24 23:50:40 +0200

    humidity model tests shouldn't pass, model is not implemented

--- src/main/java/API/service/HumidityService.java
+++ src/main/java/API/service/HumidityService.java
@@ -7,13 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HumidityService {
-    public List<Humidity> search(String home_mac) {
+    public List<Humidity> search(String home_mac) throws Exception{
         return new ArrayList<Humidity>();
     }
 
-
-
-    public List<Humidity> getLastHumidity(String home_mac) {
+    public List<Humidity> getLastHumidity(String home_mac) throws Exception{
         return new ArrayList<Humidity>();
+    }
+
+    public Double getAverage(String home_mac, String sensor_mac, String lte, String gte)throws Exception {
+        return 0.0;
     }
 }
--- src/test/java/API/service/HumidityServiceTest.java
+++ src/test/java/API/service/HumidityServiceTest.java
@@ -1,4 +1,86 @@
 package API.service;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HumidityServiceTest {
+    /*
+    La classi service devono:
+    - interfacciarsi con la base di dati
+    - estrarre i dati
+    - rappresentare i dati estratti in strutture dati di tipo lista secondo i modelli di dati estratti
+
+     Purtroppo non riesco a fare un mock della classe RestHighLevelClient poichè dichiarata final
+     quindi verificherò che i metodi della classe service rilancino le eccezzioni giuste.
+
+     */
+    @Test
+    public void test_HumidityService_search_should_throw_exception_with_mac_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.search("");
+        });
+        String expectedMessage = "il mac deve essere un mac valido";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void test_HumidityService_lasts_should_throw_exception_with_mac_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.getLastHumidity("");
+        });
+        String expectedMessage = "il mac deve essere un mac valido";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void test_HumidityService_getAverage_should_throw_exception_with_home_mac_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.getAverage("","B8:27:EB:B7:E3:4C","2020-08-23T21:59:59.999Z","2020-08-23T21:59:59.999Z");
+        });
+        String expectedMessage = "il mac o il timestamp devono essere validi";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void test_HumidityService_getAverage_should_throw_exception_with_sensor_mac_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.getAverage("B8:27:EB:B7:E3:4C","","2020-08-22T22:00:00.000Z","2020-08-23T21:59:59.999Z");
+        });
+        String expectedMessage = "il mac o il timestamp devono essere validi";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void test_HumidityService_getAverage_should_throw_exception_with_timestamp_lte_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.getAverage("B8:27:EB:B7:E3:4C","B8:27:EB:B7:E3:4C","","2020-08-23T21:59:59.999Z");
+        });
+        String expectedMessage = "il mac o il timestamp devono essere validi";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+    @Test
+    public void test_HumidityService_getAverage_should_throw_exception_with_timestamp_gte_string_empty() {
+        HumidityService service=new HumidityService();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.getAverage("B8:27:EB:B7:E3:4C","B8:27:EB:B7:E3:4C","2020-08-23T21:59:59.999Z","");
+        });
+        String expectedMessage = "il mac o il timestamp devono essere validi";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
